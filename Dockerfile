fc4eb056f4e718af76fb1c5ba27023699813f9d3
Laurence <laurence.jones@live.co.uk>
2023-03-14 14:34:33 +0000

    Add extra options

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ RUN apk update && apk add curl && apk add jq
 
 COPY docker_start.sh /docker_start.sh
 
+COPY nginx/default.conf /etc/nginx/conf.d/default.conf
+
 ENTRYPOINT ["/bin/sh", "/docker_start.sh"]
--- docker_start.sh
+++ docker_start.sh
@@ -8,6 +8,39 @@ if [ -z "$CROWDSEC_API_URL" ]; then
     exit 1
 fi
 
+if [ -z "$AUTH_TYPE" ]; then
+    echo "AUTH_TYPE is not set, defaulting to none"
+else
+    if [ "$AUTH_TYPE" = "basic" ]; then
+	if [ -z "$USERNAME" ]; then
+	    echo "USERNAME is not set"
+	    exit 1
+	fi
+	if [ -z "$PASSWORD" ]; then
+	    echo "PASSWORD is not set"
+	    exit 1
+	fi
+	htpasswd -b -c /etc/nginx/.htpasswd "$USERNAME" "$PASSWORD"
+	sed -i "s/#auth_basic/auth_basic/g" /etc/nginx/conf.d/default.conf
+	sed -i "s/#auth_basic_user_file/auth_basic_user_file/g" /etc/nginx/conf.d/default.conf
+    elif [ "$AUTH_TYPE" = "ip_based" ]; then
+	if [ -z "$IP" ]; then
+	    echo "AUTH_TYPE is set to ip_based but no IP is not set"
+	    exit 1
+	fi
+	for ip in $(echo $IP | sed "s/,/ /g")
+	do
+	    echo "allow $ip;" >> /etc/nginx/conf.d/allow.conf
+	done
+	echo "deny all;" >> /etc/nginx/conf.d/allow.conf
+    elif [ "$AUTH_TYPE" = "none" ]; then
+	echo "AUTH_TYPE is set to none, no authentication will be used"
+    else
+	echo "AUTH_TYPE is not set to basic, ip_based or none"
+	exit 1
+    fi
+fi
+
 /bin/sh -c "while true; do /usr/bin/curl -s -H 'X-Api-Key: $CROWDSEC_API_KEY' $CROWDSEC_API_URL/v1/decisions | /usr/bin/jq -r '.[].value' > /usr/share/nginx/html/list.txt ; sleep 10; done"&
 
 nginx -g "daemon off;"
