d3636b77fb761aa6f17fc010858d8eed2515f2f5
Laurence <laurence.jones@live.co.uk>
2023-03-14 18:38:43 +0000

    We need to check if the file exists first

--- nginx-list.sh
+++ nginx-list.sh
@@ -3,7 +3,10 @@ while true;
 do
     RES=$(/usr/bin/curl -s -H "X-Api-Key: $CROWDSEC_API_KEY" "$CROWDSEC_API_URL/v1/decisions");
     /usr/bin/jq --jsonargs -r '.[].value' "$RES" > /tmp/list.txt;
-    if [ "$(md5sum /tmp/list.txt | awk '{print $1}')" != "$(md5sum /usr/share/nginx/html/list.txt | awk '{print $1}')" ]; then
+    #check if /usr/share/nginx/html/list.txt exists
+    if [ ! -f /usr/share/nginx/html/list.txt ]; then
+        mv /tmp/list.txt /usr/share/nginx/html/list.txt ;
+    elif [ "$(md5sum /tmp/list.txt | awk '{print $1}')" != "$(md5sum /usr/share/nginx/html/list.txt | awk '{print $1}')" ]; then
         mv /tmp/list.txt /usr/share/nginx/html/list.txt ;
     fi
     sleep 10;
